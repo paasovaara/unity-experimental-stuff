622ca8a810166712ef3fdee8c2693679d403bbe0
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-01-10 23:02:35 +0200

    Swap testcube material runtime on keypress

---
+++ Assets/Scripts/MaterialController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialController : MonoBehaviour {
+
+    [SerializeField]
+    private Material _Idle;
+
+    [SerializeField]
+    private Material _OnPress;
+
+
+    // Use this for initialization
+    void Start () {
+        setMaterial(_Idle);
+	}
+	
+    private void setMaterial(Material m) {
+        GetComponent<Renderer>().material = m;
+    }
+
+	// Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            setMaterial(_OnPress);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space)) {
+            setMaterial(_Idle);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Unity Experiments
 
 Trying out some of the more advanced Unity features, such as shaders, physics, lights, etc.
+
+## Resources
+
+Some of the textures have been downloaded from http://www.mayang.com, Copyright © Mayang Murni Adnin, 2001-2012.
+
+http://www.mayang.com/textures/Metal/images/Patterned%20Metal/diagonal_metal_6190193.JPG
